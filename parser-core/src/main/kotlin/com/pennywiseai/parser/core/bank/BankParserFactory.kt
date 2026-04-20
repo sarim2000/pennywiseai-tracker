99e8b7015fa6af06921667b09fdfc7f5618c152f
sarim2000 <sarimbleedblue@gmail.com>
2026-04-20 23:56:57 +0530

    feat: add Greater Bank SMS parser

--- parser-core/src/main/kotlin/com/pennywiseai/parser/core/bank/BankParserFactory.kt
+++ parser-core/src/main/kotlin/com/pennywiseai/parser/core/bank/BankParserFactory.kt
@@ -103,7 +103,8 @@ object BankParserFactory {
         SNBAlAhliBankParser(),  // Saudi National Bank / Al Ahli Bank (Saudi Arabia)
         STCBankParser(),  // STC Bank (Saudi Arabia)
         MBankCZParser(),  // mBank CZ (Czech Republic)
-        BankMuscatParser()  // Bank Muscat (Oman)
+        BankMuscatParser(),  // Bank Muscat (Oman)
+        GreaterBankParser()  // Greater Bank (India)
         // Add more bank parsers here as we implement them
     )
 
